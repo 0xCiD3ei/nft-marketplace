be37fd0b20879fab920165df3bf5b31c38d8874e
Duy Tiến <duytienabc123@gmail.com>
2023-07-16 22:43:43 +0700

    :package: add thirdweb & hardhat package

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,22 +1,38 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@matterlabs/hardhat-zksync-solc");
 
 /** @type import('hardhat/config').HardhatUserConfig */
-
-const PRIVATE_KEY = "8d2632d335e7de2c2bb87d89f62e106c876dc3fd90b58d66be3450c63a72f510";
-const RPC_URL = "https://rpc.ankr.com/polygon_mumbai";
 module.exports = {
-  defaultNetwork: "polygon_mumbai",
-  networks: {
-    hardhat: {
-      chainId: 80001,
-    },
-    polygon_mumbai: {
-      url: "https://rpc.ankr.com/polygon_mumbai",
-      accounts: [`0x${PRIVATE_KEY}`],
+  zksolc: {
+    version: "1.3.9",
+    compilerSource: "binary",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
     },
   },
+  networks: {
+    zksync_testnet: {
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "goerli",
+      chainId: 280,
+      zksync: true,
+    },
+    zksync_mainnet: {
+      url: "https://zksync2-mainnet.zksync.io/",
+      ethNetwork: "mainnet",
+      chainId: 324,
+      zksync: true,
+    },
+  },
+  paths: {
+    artifacts: "./artifacts-zk",
+    cache: "./cache-zk",
+    sources: "./contracts",
+    tests: "./test",
+  },
   solidity: {
-    version: "0.8.9",
+    version: "0.8.17",
     settings: {
       optimizer: {
         enabled: true,
