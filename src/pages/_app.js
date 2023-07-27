ef39224b904d4b6d1d52262af8d72246a0901c33
Nguyễn Duy Tiến <duytienabc123@gmail.com>
2023-07-27 15:33:49 +0700

    :wrench: add auth provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,13 +3,14 @@ import {persistor, store} from "src/redux/store";
 import {PersistGate} from "redux-persist/integration/react";
 import {ToastContainer} from "react-toastify";
 import {ChainId, ThirdwebProvider} from "@thirdweb-dev/react";
+import {NFTMarketplaceProvider} from "src/context/NFTMarketplaceContext";
+import AuthProvider from "src/components/providers/AuthProvider";
 
 import "src/styles/index.scss";
 import 'src/styles/globals.css';
 import "src/assets/fonts/line-awesome-1.3.0/css/line-awesome.css";
 import "rc-slider/assets/index.css";
 import 'react-toastify/dist/ReactToastify.css';
-import {NFTMarketplaceProvider} from "src/context/NFTMarketplaceContext";
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
@@ -18,9 +19,11 @@ export default function App({ Component, pageProps }) {
     <ThirdwebProvider activeChain={ChainId.Mumbai}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <NFTMarketplaceProvider>
-            {getLayout(<Component {...pageProps} />)}
-          </NFTMarketplaceProvider>
+          <AuthProvider>
+            <NFTMarketplaceProvider>
+              {getLayout(<Component {...pageProps} />)}
+            </NFTMarketplaceProvider>
+          </AuthProvider>
         </PersistGate>
         <ToastContainer
           position="bottom-right"
